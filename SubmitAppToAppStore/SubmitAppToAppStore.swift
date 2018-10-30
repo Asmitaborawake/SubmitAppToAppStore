0514ebc0bbc8f39ddc1558b7ffac4860e4defb98
Asmita Borawake <asmita.borawake@innoplexus.com>
2018-10-30 15:13:02 +0530

    push code

---
+++ SubmitAppToAppStore/SubmitAppToAppStore.swift
@@ -0,0 +1,45 @@
+//
+//  SubmitAppToAppStore.swift
+//  SubmitAppToAppStore
+//
+//  Created by asmita.borawake on 30/10/18.
+//  Copyright © 2018 asmita.borawake. All rights reserved.
+//
+
+import Foundation
+
+
+class SubmitAppToAppStore:NSObject {
+    
+    //Steps To Submit App To App Store
+    //
+    //1. Add app icon to ur app
+   // 2. Sign to ur apple account
+   // 3. Goto Certificates , IDS, Profiles
+   // 4. Goto App IDs
+   // 5. Click on + button Register iOS App IDS :
+   //6.  Add App ID Description I.e ur app name
+   //7. Add Explicit App ID i.e ur app bundle identifier
+   // 8. If any app service select that like push notification etc.
+   // 9. Click on continue button , register complete click on done
+   // 10. Ur iOS app IDS is generated
+   // 11. Now go to Provisioning Profile select distribution
+   // 12. Select + button , select distribution profile for App Store click continue
+  //  13. Select App ID : select just created app id , click continue
+  //  14. Select Certificates , name this provising profile , click done.
+  //  15. Go to iTunes account :
+  //  16. In My app click + button , name the app fill all details then ur app is created fill all required details
+  //  17. Again come back to ur Xcode select generic device , product - Archiving
+  //  18. I will go to archives then click on verify , once verified click on done
+  //  19. select Upload app to App Store , verify & click Upload button , click on done
+  //  20. It will take some minutes to reflect in itunes account , till then fill all details like description of ur app ,Keyword , add app icon ,copy right , add app review information ,
+  //   21. Add app screenshots
+  //   22. click Select build before submitting : select your build
+  //   23. Select the save button
+  //  24. Lastly select Submit for Review button.
+    
+    //Link of Submit App to AppStore
+    //https://www.youtube.com/watch?v=tnbOcpwJGa8
+
+    
+}
